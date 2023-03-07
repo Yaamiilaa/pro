b80c43da1a7e22c429c2df07cc3de1a9a23f232f
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-03-07 10:59:46 +0000

    Se realiza la tarea pangram.py

--- ut4/ejer/pangram.py
+++ ut4/ejer/pangram.py
@@ -6,8 +6,6 @@
 def is_pangram(text: str) -> bool:
     ALPHA = set("abcdefghijklmnopqrstuvwxyz")
     text = set(''.join(text.lower().split()))
-    print(ALPHA)
-    print(text)
     if text == ALPHA:
         return True
     else:
