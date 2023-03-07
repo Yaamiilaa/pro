b112a3306221a8ea3b95a2d0a256380b96095f73
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-03-07 11:19:03 +0000

    Realización del ejercicio cycle_alphabet.py

--- ut4/ejer/cycle_alphabet.py
+++ ut4/ejer/cycle_alphabet.py
@@ -4,11 +4,19 @@
 
 
 def run(max_letters: int) -> str:
-    # TU CÓDIGO AQUÍ
-    text = 'output'
-
+    cycle_alphabet = []
+    ALPHA = list("abcdefghijklmnopqrstuvwxyz")
+    for i in range(max_letters):   
+        if ALPHA[i] == 'z':
+            ALPHA[-1] = ALPHA[0]
+            cycle_alphabet.append(ALPHA[i])
+            
+            
+ 
+    text = "".join(cycle_alphabet)
+    print(text)
     return text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(0)
