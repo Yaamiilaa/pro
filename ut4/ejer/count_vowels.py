e8f32c82bc01de4a1d3f0b2b7c68b8d5688bb74a
yamila <yamilaramoshdez@gmail.com>
2023-03-09 17:49:17 +0000

    Realización del ejercicio count_vowles.py

---
+++ ut4/ejer/count_vowels.py
@@ -0,0 +1,14 @@
+# *******************************
+# CONTANDO VOCALES (EN RECURSIVO)
+# *******************************
+
+# 
+
+
+def count_vowels(text: str):
+    VOWELS = 'aeiou'
+    if len(text) == 1:
+        if text[0] in VOWELS:
+            return 1
+        return 0
+    return count_vowels(text[0]) + count_vowels(text[1:])
