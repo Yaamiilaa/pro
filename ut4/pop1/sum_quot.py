301e5208a7e1e99f6d926983707c1134004d6322
yamila <yamilaramoshdez@gmail.com>
2023-03-16 18:30:58 +0000

    Corrección del examen

--- ut4/pop1/sum_quot.py
+++ ut4/pop1/sum_quot.py
@@ -4,6 +4,6 @@
 
 
 def sum_quot(n: int) -> float:
-    if n < 0:
-        return 0
-    return (1 / sum_quot(n)) + (1 / sum_quot(n - 1))
+    if n == 1:
+        return 1
+    return 1 / n + sum_quot(n - 1)
