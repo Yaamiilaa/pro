0c6fdfd4560ae1a66cfa3a5186bd7457ecf10ff4
yamila <yamilaramoshdez@gmail.com>
2023-04-14 17:15:08 +0100

    Modificación del ejercicio date.oy ; git add .

--- ut5/ejer/date.py
+++ ut5/ejer/date.py
@@ -1,17 +1,17 @@
 class Date:
     DAYS_IN_MONTH = {
-        "enero": 31,
-        "febrero": 28,
-        "marzo": 31,
-        "abril": 30,
-        "mayo": 31,
-        "junio": 30,
-        "julio": 31,
-        "agosto": 31,
-        "septiembre": 30,
-        "octubre": 31,
-        "noviembre": 30,
-        "diciembre": 31,
+        1: ["enero", 31],
+        2: ["febrero", 28],
+        3: ["marzo", 31],
+        4: ["abril", 30],
+        5: ["mayo", 31],
+        6: ["junio", 30],
+        7: ["julio", 31],
+        8: ["agosto", 31],
+        9: ["septiembre", 30],
+        10: ["octubre", 31],
+        11: ["noviembre", 30],
+        12: ["diciembre", 31],
     }
     DAYS_IN_WEEK = {
         "domiengo": 0,
@@ -40,17 +40,13 @@ class Date:
         divby400 = self.year % 400 == 0
         return divby4 or divby400
 
-    # INTENTAR HACER DECORADOR PARA COMPROBAR SI LA FECHA INTRODUCIDA ESTA ENTRE LAS FECHAS INDICADAS. (CUIDADO CON EL MES DE FEBRERO QUE NUNCA TIENE 31 DIAS, PARA ESO HAY QUE PONER QUE SI EL MES 2
-    # TIENE MENOS DE 28 EN EL CASO DEL AÑO NO BISIESTO) TAMBIÉN HAY QUE TENER EN CUENTA QUE LOS MESES DE 30 TAMPOCO VAN A TENER 31 DIAS.
-    # SI EL AÑO ES BISIESTO HAYQ UE CAMBIAR EN EL DICCIONARIO DAYS_IN_MONTH EL MES DE FEBRERO A 29 DÍAS.
-
-    def verify_correct_date(self, month: str):
+    def verify_correct_date(self) -> bool:
         if (
             self.day > Date.CORRECT_YEARS["day"][0]
             and self.day < Date.CORRECT_YEARS["day"][1]
         ):
             if not self.is_leap_year():
-                if self.day > Date.DAYS_IN_MONTH[month]:
+                if self.day > Date.DAYS_IN_MONTH[self.month][1]:
                     self.day = 1
                 elif self.month > len(Date.DAYS_IN_MONTH):
                     self.month = 1
@@ -59,19 +55,30 @@ class Date:
                     and self.year > Date.CORRECT_YEARS["year"][1]
                 ):
                     self.year = 1900
+                return True
+            if self.month == 2:
+                Date.DAYS_IN_MONTH[2][1] = 29
+                return False
 
     def days_in_month(self) -> int:
-        pass
+        if self.is_leap_year():
+            if self.month == 2:
+                Date.DAYS_IN_MONTH[2][1] = 29
+        days_in_month = Date.DAYS_IN_MONTH[self.month][1]
+        return days_in_month
 
+    # Hay que sumar solos días pero hay que mirar el número de días que tiene un mes y si el día es 15
+    # de enero solo hay que sumarle 15 y no los 31 días. Utilizar módulo con el desplazamiento modular
     def delta_days(self) -> int:
         """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
-        pass
+        days_passed = 0
 
     def weekday(self) -> int:
         """día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
         El 1-1-1900 fue domingo."""
         pass
 
+    # Para ello hayq ue hacer el el método weekday y ver si el día es sabado o domingo
     def is_weekend(self) -> bool:
         pass
 
@@ -83,8 +90,39 @@ class Date:
         """martes 2 de septiembre de 2003"""
         pass
 
-    # operador + suma días a la fecha
-    # operador - resta días a la fecha o calcula la diferencia entre dos fechas
-    # operador == dice si dos fechas son iguales
-    # operador > dice si una fecha es mayor que otra
-    # operador < dice si una fecha es menor que otra
+    def __eq__(self, other) -> bool:
+
+        return (
+            (self.day == other.day)
+            and (self.month == other.month)
+            and (self.year == other.year)
+        )
+
+    def __gt__(self, other):
+        return (
+            (self.day > other.day)
+            and (self.month > other.month)
+            and (self.year > other.year)
+        )
+
+    def __lt__(self, other):
+        return (
+            (self.day < other.day)
+            and (self.month < other.month)
+            and (self.year < other.year)
+        )
+
+    # operador + suma días a la fecha. Al sumar dos fechas tenemos que ver que llegue hasta el límite de
+    # días que tiene el mes, y si se pasa de días hay que sumarselo al mes y si no al año. Una vez que se pase
+    # del año que nos indica en el constructor tenemos que aplicar el método verify_correct_date().
+    # Utilizar el módulo con el desplazamiento modular.
+
+    # operador - resta días a la fecha o calcula la diferencia entre dos fechas. Comprobar si el dato
+    # introducido (other) es de tipo entero date. Si es de tipo date hay que volver la resta de tipo
+    # date y si es enterio hay que devolver un entero.
+
+    # operador == dice si dos fechas son iguales.
+
+    # operador > dice si una fecha es mayor que otra. Para ello debemos comparar todos los datos introducidos
+
+    # operador < dice si una fecha es menor que otra. Lo mismo que el operador de arriba.
