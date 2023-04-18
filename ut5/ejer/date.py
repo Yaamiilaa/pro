06cb2be830f243d32f3a02f8e5f78cd994059a65
Yaamiilaa <113527458+Yaamiilaa@users.noreply.github.com>
2023-04-18 18:56:21 +0100

    Modificación del ejercicio date.py

--- ut5/ejer/date.py
+++ ut5/ejer/date.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Date:
     DAYS_IN_MONTH = {
         1: ["enero", 31],
@@ -14,13 +17,13 @@ class Date:
         12: ["diciembre", 31],
     }
     DAYS_IN_WEEK = {
-        "domiengo": 0,
-        "lunes": 1,
-        "martes": 2,
-        "miercoles": 3,
-        "jueves": 4,
-        "viernes": 5,
-        "sabado": 6,
+        "domingo": 1,
+        "lunes": 2,
+        "martes": 3,
+        "miercoles": 4,
+        "jueves": 5,
+        "viernes": 6,
+        "sabado": 7,
     }
 
     CORRECT_YEARS = {"day": [1, 31], "month": [1, 12], "year": [1900, 2050]}
@@ -29,100 +32,91 @@ class Date:
         """Validar día, mes y año. Se comprobará si la fecha es correcta
         (entre el 1-1-1900 y el 31-12-2050); si el día no es correcto, lo pondrá a 1;
         si el mes no es correcto, lo pondrá a 1; y si el año no es correcto, lo pondrá a 1900.
-        Ojo con los años bisiestos.
-        """
+        Ojo con los años bisiestos."""
         self.day = day
         self.month = month
         self.year = year
+        if (
+            self.day < Date.CORRECT_YEARS["day"][0]
+            or self.day > Date.CORRECT_YEARS["day"][1]
+        ):
+            self.day = 1
+        if self.month > len(Date.DAYS_IN_MONTH):
+            self.month = 1
+        if (
+            self.year < Date.CORRECT_YEARS["year"][0]
+            or self.year > Date.CORRECT_YEARS["year"][1]
+        ):
+            self.year = 1900
 
-    def is_leap_year(self) -> bool:
-        divby4 = (self.year % 4 == 0) and (self.year % 100 != 0)
-        divby400 = self.year % 400 == 0
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
+        divby4 = (year % 4 == 0) and (year % 100 != 0)
+        divby400 = year % 400 == 0
         return divby4 or divby400
 
-    def verify_correct_date(self) -> bool:
-        if (
-            self.day > Date.CORRECT_YEARS["day"][0]
-            and self.day < Date.CORRECT_YEARS["day"][1]
-        ):
-            if not self.is_leap_year():
-                if self.day > Date.DAYS_IN_MONTH[self.month][1]:
-                    self.day = 1
-                elif self.month > len(Date.DAYS_IN_MONTH):
-                    self.month = 1
-                elif (
-                    self.year < Date.CORRECT_YEARS["year"][0]
-                    and self.year > Date.CORRECT_YEARS["year"][1]
-                ):
-                    self.year = 1900
-                return True
-            if self.month == 2:
-                Date.DAYS_IN_MONTH[2][1] = 29
-                return False
+    def get_delta_days(self) -> int:
+        """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
+        ...
 
+    @property
     def days_in_month(self) -> int:
-        if self.is_leap_year():
+        """Día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
+        El 1-1-1900 fue domingo."""
+        if self.is_leap_year(self.year):
             if self.month == 2:
                 Date.DAYS_IN_MONTH[2][1] = 29
         days_in_month = Date.DAYS_IN_MONTH[self.month][1]
         return days_in_month
 
-    # Hay que sumar solos días pero hay que mirar el número de días que tiene un mes y si el día es 15
-    # de enero solo hay que sumarle 15 y no los 31 días. Utilizar módulo con el desplazamiento modular
-    def delta_days(self) -> int:
-        """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
-        days_passed = 0
-
+    @property
     def weekday(self) -> int:
-        """día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
-        El 1-1-1900 fue domingo."""
         pass
 
-    # Para ello hayq ue hacer el el método weekday y ver si el día es sabado o domingo
+    @property
     def is_weekend(self) -> bool:
         pass
 
+    @property
     def short_date(self) -> str:
         """02/09/2003"""
-        pass
+        if self.day < 10:
+            self.day = f"0{self.day}"
+        if self.month < 10:
+            self.month = f"0{self.month}"
+        return f"{self.day}/{self.month}/{self.year}"
 
     def __str__(self):
-        """martes 2 de septiembre de 2003"""
+        """MARTES 2 DE SEPTIEMBRE DE 2003"""
         pass
 
-    def __eq__(self, other) -> bool:
+    def __add__(self, days: int) -> Date:
+        """Sumar un número de días a la fecha"""
+        pass
 
+    def __sub__(self, other: Date | int) -> int | Date:
+        """Dos opciones:
+        1) Restar una fecha a otra fecha -> Número de días
+        2) Restar un número de días la fecha -> Nueva fecha"""
+        pass
+
+    def __eq__(self, other: Date) -> bool:
         return (
             (self.day == other.day)
             and (self.month == other.month)
             and (self.year == other.year)
         )
 
-    def __gt__(self, other):
+    def __gt__(self, other: Date) -> bool:
         return (
             (self.day > other.day)
             and (self.month > other.month)
             and (self.year > other.year)
         )
 
-    def __lt__(self, other):
+    def __lt__(self, other: Date) -> bool:
         return (
             (self.day < other.day)
             and (self.month < other.month)
             and (self.year < other.year)
         )
-
-    # operador + suma días a la fecha. Al sumar dos fechas tenemos que ver que llegue hasta el límite de
-    # días que tiene el mes, y si se pasa de días hay que sumarselo al mes y si no al año. Una vez que se pase
-    # del año que nos indica en el constructor tenemos que aplicar el método verify_correct_date().
-    # Utilizar el módulo con el desplazamiento modular.
-
-    # operador - resta días a la fecha o calcula la diferencia entre dos fechas. Comprobar si el dato
-    # introducido (other) es de tipo entero date. Si es de tipo date hay que volver la resta de tipo
-    # date y si es enterio hay que devolver un entero.
-
-    # operador == dice si dos fechas son iguales.
-
-    # operador > dice si una fecha es mayor que otra. Para ello debemos comparar todos los datos introducidos
-
-    # operador < dice si una fecha es menor que otra. Lo mismo que el operador de arriba
