87ea1a8256d9241e050adb84031359c8dec0ca06
yamila <yamilaramoshdez@gmail.com>
2023-04-25 19:57:52 +0100

    Se realiza el ejercicio de fibonacci.py

---
+++ ut5/ejer/fibonacci_iterable.py
@@ -0,0 +1,23 @@
+class Fibonacci:
+    def __init__(self, num: int):
+        self.num = num
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if 
+        a = 0
+        print(a)
+        b = 1
+        print(b)
+
+        for _ in range(self.num - 2):
+            r = a + b
+            a = b
+            b = r
+            print(r)
+
+
+for nums in Fibonacci(1):
+    print(nums)
