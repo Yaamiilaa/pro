cc05a1af79ac639bd8f2af011309a2bb1814353f
yamila <yamilaramoshdez@gmail.com>
2023-04-25 18:49:14 +0100

    Se realiza el ejercicio infinitylist.py

---
+++ ut5/ejer/infinitylist.py
@@ -0,0 +1,23 @@
+class InfinityList:
+    def __init__(self, *values: int, fill_value=None):
+        self.values = []
+        self.fill_value = fill_value
+        for value in values:
+            self.values.append(value)
+
+    def __getitem__(self, index: int):
+        return self.values[index]
+
+    def __len__(self):
+        return len(self.values)
+
+    def __setitem__(self, index: int, value: int):
+        if index > len(self.values):
+            for i in range(len(self.values), index + 1):
+                self.values.append(self.fill_value)
+        self.values[index] = value
+
+
+h = InfinityList(5, 5, 6, 8)
+h[10] = 20
+print(h.values)
