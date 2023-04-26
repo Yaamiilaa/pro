81e802f3b16ff704d0b0be070190919b8776b07b
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-04-26 11:22:52 +0100

    Se modifica el ejercicio de fibonacci_iterable.py

--- ut5/ejer/fibonacci_iterable.py
+++ ut5/ejer/fibonacci_iterable.py
@@ -1,22 +1,35 @@
-class Fibonacci:
-    def __init__(self, num: int):
-        self.num = num
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        a = 0
-        print(a)
-        b = 1
-        print(b)
-
-        for _ in range(self.num - 2):
-            r = a + b
-            a = b
-            b = r
-            print(r)
+# ******************
+# FIBONACCI ITERABLE
+# ******************
 
 
-for nums in Fibonacci(1):
-    print(nums)
+def run():
+    class Fibonacci:
+        def __init__(self):
+            self.a = 0
+            self.b = 1
+            
+
+        def __str__(self):
+            return f'{self.a} {self.b} {self.value}'
+        
+
+    class FibonacciIterable:
+        def __init__(self, num):
+            self.num = num
+            self.pointer = 0
+            self.nums = [Fibonacci(i) for i in range(self.num)]
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.pointer >= self.num:
+                raise StopIteration
+            
+            nums = self.nums[self.pointer]
+            self.pointer += 1
+            return nums
+
+    for nums in FibonacciIterable(1):
+        print(nums)
