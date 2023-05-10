1a757dc606ff9beaeec322fa69e0fac0845d0778
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-05-10 17:47:02 +0100

    Se modifica el ejercicio value_error.py

--- ut5/ejer/value_error.py
+++ ut5/ejer/value_error.py
@@ -1,4 +1,5 @@
-def getint():
+# Version iterativa
+def getint_iterative():
     while True:
         try:
             value = int(input("Give me an integer number: "))
@@ -6,3 +7,12 @@ def getint():
                 break
         except ValueError:
             print("Not a valid integer. Try it again!")
+
+
+# Version recursiva
+def getint_recursive():
+    try:
+        value = int(input("Give me an integer number: "))
+    except ValueError:
+        print("Not a valid integer. Try it again!")
+        return getint_recursive()
