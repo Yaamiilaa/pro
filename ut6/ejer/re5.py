e3cf6831bde3fa4d5ef4237d12e16f410e31e06c
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-05-24 17:56:06 +0100

    Se modifica el ejercicio 5 de expresiones regulares

--- ut6/ejer/re5.py
+++ ut6/ejer/re5.py
@@ -1,28 +1,33 @@
-# Escriba un programa en Python que obtenga el 
-# resultado de una operación entre números enteros 
-# positivos. Las operación puede ser suma, resta, 
+# Escriba un programa en Python que obtenga el
+# resultado de una operación entre números enteros
+# positivos. Las operación puede ser suma, resta,
 # multiplicación o división, y puede haber espacios
 # (o no) entre los operandos y el operador.
 
 import re
 
-def get_result(numbers: int):
-    regex = r'(?P<num1>\d+)\s*(?P<operator>[\+\-\*\/])\s*(?P<num2>\d+)'
+
+def get_result(numbers: str) -> int | float:
+    regex = r"(?P<num1>\d+)\s*(?P<operator>[\+\-\*\/])\s*(?P<num2>\d+)"
     groups = re.search(regex, numbers)
-    num1 = int(groups['num1'])
-    num2 = int(groups['num2'])
-    operator = groups['operator']
+    num1 = int(groups["num1"])
+    num2 = int(groups["num2"])
+    operator = groups["operator"]
     match operator:
-        case '+':
+        case "+":
             result = num1 + num2
-        case '-':
+        case "-":
             result = num1 - num2
-        case '*':
+        case "*":
             result = num1 * num2
-        case '/':
+        case "/":
             result = num1 / num2
     return result
 
-print(get_result('''2                         
-                  /          2'''))
 
+print(
+    get_result(
+        """2                         
+                  /          2"""
+    )
+)
