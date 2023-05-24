3dfb79dcd921a900222b0b6417dd8de5f56d0b60
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-05-24 19:34:51 +0100

    Se realizan cambios en los ejercicios de expresiones regulares

--- ut6/ejer/re4.py
+++ ut6/ejer/re4.py
@@ -1,11 +1,13 @@
-# Escriba un programa en Python que determine si un email dado 
+# Escriba un programa en Python que determine si un email dado
 # tiene el formato correcto.
 
 import re
 
+
 def is_an_email_valid(email: str) -> bool:
-    regex = r'\w@\w.\w'
+    regex = r"\w@\w.(es|com)"
     result = re.search(regex, email, re.I)
     return result is not None
 
-print(is_an_email_valid('ram$Yamila.2003@HOTMAIL.ES'))
+
+print(is_an_email_valid("ramosyamila2003@gmail.es"))
