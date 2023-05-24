8e2cb084fe50bf15d954b5d0121b409ba06da5c9
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-05-24 10:17:55 +0100

    Se modifica el ejercicio 2 de re

---
+++ ut6/ejer/re2.py
@@ -0,0 +1,16 @@
+# Escriba un programa en Python que encuentre todas las URLs en un texto dado.
+
+import re
+
+
+def find_url(url):
+    regex = r"\b[https$]\S*[^,\s\w]*"
+    result = re.findall(regex, url)
+    return result
+
+
+print(
+    find_url(
+        """Esta es mi url: https://aprendepython.es/stdlib/text_processing/re/ Esta es mi otra url: https://www.google.com/search?client=firefox-b-e&q=que+caracteres+no+est%C3%A1n+permitidos+en+una+url"""
+    )
+)
