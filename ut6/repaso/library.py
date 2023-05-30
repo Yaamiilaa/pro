14293b76a1305f47c1cad68466bfb515d51fec2f
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-05-30 17:55:01 +0100

    Se realizan varios ejercicios de repaso

---
+++ ut6/repaso/library.py
@@ -0,0 +1,19 @@
+# Crea una clase llamada "Libro" que represente un libro en una tienda. La clase debe tener los siguientes atributos:
+
+# Título: el título del libro.
+# Autor: el autor del libro.
+# Precio: el precio del libro.
+# Stock: la cantidad de ejemplares disponibles en el stock.
+# La clase debe tener los siguientes métodos:
+
+# actualizar_precio(nuevo_precio): recibe un nuevo valor numérico que representa el precio actualizado del libro y lo actualiza.
+
+# vender_copia(): reduce en 1 la cantidad de ejemplares disponibles en el stock. Si no hay ejemplares disponibles, muestra un mensaje 
+# indicando que el libro está agotado.
+
+# Crea un programa principal en el que instancies objetos de la clase "Libro" y realices las siguientes acciones:
+from random import randint 
+
+
+
+
