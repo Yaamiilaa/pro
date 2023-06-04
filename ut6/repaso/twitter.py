e760447394234f86cad9df85af814cc78c5ac6cb
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-06-04 23:19:27 +0100

    Se realiza el ejercicio de twitter.py de repaso

--- ut6/repaso/twitter.py
+++ ut6/repaso/twitter.py
@@ -3,32 +3,34 @@ from __future__ import annotations
 import re
 import sqlite3
 
-DB_PATH = 'twitter.db'
+DB_PATH = "twitter.db"
 
-TWEET_EMOJI = '🐦'
-RETWEET_EMOJI = '🔁'
+TWEET_EMOJI = "🐦"
+RETWEET_EMOJI = "🔁"
 MAX_TWEET_LENGTH = 280
 
 
 def create_db(db_path: str = DB_PATH) -> None:
-    '''Crea la base de datos y las siguientes tablas:
+    """Crea la base de datos y las siguientes tablas:
     - user (id, username, password, bio)
     - tweet (id, content, user_id, retweet_from)
         └ user_id es clave ajena de user(id)
-        └ retweet_from es clave ajena de tweet(id)'''
-    table_user = '''CREATE TABLE user(
+        └ retweet_from es clave ajena de tweet(id)"""
+    table_user = """CREATE TABLE user(
     id INTEGER PRYMARY KEY,
     username TEXT,
-    bio TEXT)'''
+    password TEXT,
+    bio TEXT)"""
 
-    table_tweet = '''CREATE TABLE tweet(
+    table_tweet = """CREATE TABLE tweet(
         id PRIMARY KEY,
         content TEXT,
         user_id INTEGER,
-        retweet_id INTEGER,
+        retweet_from INTEGER,
         FOREIGN KEY (user_id) REFERENCES user(id),
-        FOREIGN KEY (retweet_id) REFERENCES tweet(id))'''
+        FOREIGN KEY (retweet_from) REFERENCES tweet(id))"""
     con = sqlite3.connect(db_path)
+    con.row_factory = sqlite3.Row
     cur = con.cursor()
     cur.execute(table_user)
     cur.execute(table_tweet)
@@ -36,109 +38,130 @@ def create_db(db_path: str = DB_PATH) -> None:
 
 
 class User:
-    def __init__(self, username: str, password: str, bio: str = '', user_id: int = 0):
-        '''Constructor de la clase User.
+    def __init__(self, username: str, password: str, bio: str = "", user_id: int = 0):
+        """Constructor de la clase User.
         - Crea los atributos con y cur para la conexión a la base de datos (con factoría Row).
         - Crea los atributos username, password, bio, id y logged.
-        '''
-        pass
+        """
+        self.con = sqlite3.connect(DB_PATH)
+        self.con.row_factory = sqlite3.Row
+        self.cur = self.con.cursor()
+        self.username = username
+        self.password = password
+        self.bio = bio
+        self.id = user_id
+        self.logged = False
 
     def save(self) -> None:
-        '''Guarda en la base de datos un objeto de tipo User.
-        Además actualiza el atributo "id" del objeto a partir de lo que devuelve la inserción.'''
-        pass
+        """Guarda en la base de datos un objeto de tipo User.
+        Además actualiza el atributo "id" del objeto a partir de lo que devuelve la inserción."""
+        sql = "INSERT INTO user (username, password, bio) VALUES (?, ?, ?)"
+        self.cur.execute(sql, (self.username, self.password, self.bio))
+        self.con.commit()
+        self.id = self.cur.lastrowid
 
     def login(self, password: str) -> None:
-        '''Realiza el login del usuario.
+        """Realiza el login del usuario.
         Comprueba si existe este usuario con el password pasado por parámetro en la BBDD
-        y actualiza los atributos correspondientes.'''
-        pass
+        y actualiza los atributos correspondientes."""
+        sql = 'SELECT * FROM user WHERE password = ?'
+        result = self.cur.execute(sql, (password,))
+        rows = result.fetchall()
+        self.logged = True if len(rows) > 0 else False
 
     def tweet(self, content: str) -> Tweet:
-        '''Crea un tweet con el contenido indicado y lo almacena en la base de datos.
+        """Crea un tweet con el contenido indicado y lo almacena en la base de datos.
         - Utiliza el método save propio de la clase Tweet.
         - Hay que retornar el tweet creado.
         - Si el usuario no está logeado hay que lanzar una excepción de tipo TwitterError
         con el mensaje: User <usuario> is not logged in!
         - Si el tweet supera el límite de caracteres hay que lanzar una excepción de tipo
-        TwitterError con el mensaje: Tweet hasta more than 280 chars!'''
-        pass
+        TwitterError con el mensaje: Tweet hasta more than 280 chars!"""
+        
 
     def retweet(self, tweet_id: int) -> Tweet:
-        '''Crea un retweet con el contenido indicado y lo almacena en la base de datos.
+        """Crea un retweet con el contenido indicado y lo almacena en la base de datos.
         - Utiliza el método save propio de la clase Tweet.
         - Hay que retornar el tweet creado.
         - Si el usuario no está logeado hay que lanzar una excepción de tipo TwitterError
         con el mensaje: User <usuario> is not logged in!
         - Si tweet_id no existe en la base de datos hay que lanzar una excepción de tipo
-        TwitterError con el mensaje: Tweet with id <id> does not exist!'''
+        TwitterError con el mensaje: Tweet with id <id> does not exist!"""
         pass
 
     @property
     def tweets(self):
-        '''Función generadora que devuelve todos los tweets propios del usuario.
-        - Lo que se devuelven son objetos de tipo Tweet (usar el método from_db_row).'''
+        """Función generadora que devuelve todos los tweets propios del usuario.
+        - Lo que se devuelven son objetos de tipo Tweet (usar el método from_db_row)."""
         pass
 
     def __repr__(self):
-        '''Representa un usuario con el formato:
-        <usuario>: <bio>'''
-        pass
+        """Representa un usuario con el formato:
+        <usuario>: <bio>"""
+        return f'{self.username}: {self.bio}'
 
     @classmethod
     def from_db_row(cls, row: sqlite3.Row):
-        '''Crea un objeto de tipo User a partir de una fila de consulta SQL'''
-        pass
+        """Crea un objeto de tipo User a partir de una fila de consulta SQL"""
+        return User(**row)
+        
+    
 
 
 class Tweet:
-    def __init__(self, content: str = '', retweet_from: int = 0, tweet_id: int = 0):
-        '''Constructor de la clase Tweet.
+    def __init__(self, content: str = "", retweet_from: int = 0, tweet_id: int = 0):
+        """Constructor de la clase Tweet.
         - Crea los atributos con y cur para la conexión a la base de datos (con factoría Row)
         - Crea los atributos _content, retweet_from e id.
         - retweet_from indica el id del tweet que se retuitea.
           Un id válido debe ser mayor o igual que 1.
         - Si es un retweet el contenido debe ser la cadena vacía.
-        '''
-        pass
+        """
+        self.con = sqlite3.connect(DB_PATH)
+        self.con.row_factory = sqlite3.Row
+        self.cur = self.con.cursor()
+        self._content = '' if self.is_retweet else content
+        self.retweet_from = retweet_from
+        self.id = tweet_id
+
 
     @property
     def is_retweet(self) -> bool:
-        '''Indica si el tweet es un retweet.'''
-        pass
+        """Indica si el tweet es un retweet."""
+        return self.retweet_from > 0
 
     @property
     def content(self) -> str:
-        '''Devuelve el contenido del tweet.
-        - Si es un retweet el contenido habrá que buscarlo en el tweet retuiteado.'''
-        pass
+        """Devuelve el contenido del tweet.
+        - Si es un retweet el contenido habrá que buscarlo en el tweet retuiteado."""
+        
 
     def save(self, user: User) -> None:
-        '''Guarda el tweet en la base de datos.
+        """Guarda el tweet en la base de datos.
         - El parámetro user es el usuario que escribe el tweet.
-        Además actualiza el atributo "id" del objeto a partir de lo que devuelve la inserción.'''
+        Además actualiza el atributo "id" del objeto a partir de lo que devuelve la inserción."""
         pass
 
     def __repr__(self):
-        '''Representa un tweet con el formato:
-        <emoji> <content> (id=<id>)'''
+        """Representa un tweet con el formato:
+        <emoji> <content> (id=<id>)"""
         pass
 
     @classmethod
     def from_db_row(cls, row: sqlite3.Row) -> Tweet:
-        '''Crea un objeto de tipo Tweet a partir de una fila de consulta SQL'''
+        """Crea un objeto de tipo Tweet a partir de una fila de consulta SQL"""
         pass
 
 
 class Twitter:
     def __init__(self):
-        '''Constructor de la clase Twitter.
+        """Constructor de la clase Twitter.
         - Crea los atributos con y cur para la conexión a la base de datos (con factoría Row)
-        '''
+        """
         pass
 
-    def add_user(self, username: str, password: str, bio: str = '') -> User:
-        '''Crea un objeto de tipo User y lo guarda en la base de datos.
+    def add_user(self, username: str, password: str, bio: str = "") -> User:
+        """Crea un objeto de tipo User y lo guarda en la base de datos.
         - Haz uso de los métodos ya creados.
         - Hay que retornar el objeto creado.
         - La contraseña debe seguir el siguiente formato:
@@ -147,13 +170,13 @@ class Twitter:
           * Continuar con 2, 3 o 4 letras de la A-Z (incluyendo minúsculas).
           * Terminar con una exclamación o un asterisco.
         Si no sigue este formato hay que elevar una excepción de tipo TwitterError
-        con el mensaje: Password does not follow security rules!'''
+        con el mensaje: Password does not follow security rules!"""
         pass
 
     def get_user(self, user_id: int) -> User:
-        '''Devuelve el usuario con el user_id indicado.
+        """Devuelve el usuario con el user_id indicado.
         Si el usuario no existe hay elevar una excepción de tipo TwitterError con el mensaje:
-        User with id <id> does not exist!'''
+        User with id <id> does not exist!"""
         pass
 
 
